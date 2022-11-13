c0a0acc5d457f6408d0e1b40f67dc4dc6de29678
Zetaka <franvazquez1993@gmail.com>
2022-11-13 18:23:31 +0100

    Sounds
    - sonidos de pelota al golpear paredes jugadores o marcar gol

--- Pong 2D/Assets/Scripts/BallController.cs
+++ Pong 2D/Assets/Scripts/BallController.cs
@@ -9,11 +9,16 @@ public class BallController : MonoBehaviour
     [SerializeField] private float speedMultiplier = 1.1f;
     private Rigidbody2D ballRb2d;
 
+    [SerializeField] private AudioClip goal;
+    [SerializeField] private AudioClip hitplayer;
+    [SerializeField] private AudioClip hitwall;
+    private AudioSource audioSource;
 
     //Functions
     void Start()
     {
         ballRb2d = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
         InitBall();
     }
 
@@ -25,24 +30,40 @@ public class BallController : MonoBehaviour
         ballRb2d.velocity = new Vector2(xSpeed, ySpeed)* initialSpeed;
     }
 
+    private void PlayBallSound(int sound)
+    {
+        switch (sound)
+        {
+            case 1: audioSource.clip = goal;
+                break;
+            case 2: audioSource.clip = hitplayer;
+                break;
+            case 3: audioSource.clip = hitwall;
+                break;
+        }
+        audioSource.Play();        
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
+        {
             ballRb2d.velocity *= speedMultiplier;
+            PlayBallSound(2);
+        }
+        else
+        {
+            PlayBallSound(3);
+        }
+            
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("GoalLeft"))
-        {
+        if (collision.gameObject.CompareTag("GoalLeft"))        
             GameManager.Instance.player1Goal();
-          
-        }
         else
-        {
             GameManager.Instance.player2Goal();
-          
-        }
+        PlayBallSound(1);
         GameManager.Instance.Restart();
         InitBall();
     }
